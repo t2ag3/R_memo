c690a76a15c310feba189a7a2d8ae8fe9c5dd89d
Ken Takasawa <ktakazaw@ncc.go.jp>
2022-10-25 09:26:48 +0900

    add

---
+++ mytheme/R/theme_paper_bar.R
@@ -0,0 +1,36 @@
+#' @name theme_paper_bar
+#' @aliases theme_paper_bar
+#' @title Theme of ggplot2 barplot for scientific paper usage
+#' @description Theme of ggplot2 barplot for scientific paper usage.
+#' No background panel.
+#' No grid of plot area.
+#' The color of all texts are "black".
+#'
+#' @author Ken Takasawa
+#'
+#' @importFrom ggplot2
+#'    theme
+#'    element_line
+#'    element_text
+#'    element_blank
+#'
+#' @export
+#' @examples
+#' theme_paper_bar()
+
+theme_paper_bar <- function(){
+  theme(line = element_line(colour = 'black', lineend = 'square'),
+        text = element_text(color = 'black', size = 10),
+        panel.background = element_blank(),
+        plot.background = element_blank(),
+        strip.background = element_blank(),
+        legend.background = element_blank(),
+        legend.box.background = element_blank(),
+        legend.key = element_blank(),
+        axis.line = element_line(color = 'black', lineend = 'square'),
+        axis.text = element_text(color = 'black', size = 9),
+        panel.grid = element_blank(),
+        axis.ticks = element_line(color = 'black'),
+        complete = TRUE) +
+    scale_y_continuous(expand = c(0, 0))
+}
