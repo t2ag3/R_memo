3495d7fa668817a3876fa21cf3113f6e334f5c9d
Ken Takasawa <ktakazaw@ncc.go.jp>
2022-10-25 09:34:15 +0900

    add

--- mytheme/R/theme_paper.R
+++ mytheme/R/theme_paper.R
@@ -51,19 +51,7 @@ theme_paper <- function(){
 #' theme_paper_bar()
 
 theme_paper_bar <- function(){
-  theme(line = element_line(colour = 'black', lineend = 'square'),
-        text = element_text(color = 'black', size = 10),
-        panel.background = element_blank(),
-        plot.background = element_blank(),
-        strip.background = element_blank(),
-        legend.background = element_blank(),
-        legend.box.background = element_blank(),
-        legend.key = element_blank(),
-        axis.line = element_line(color = 'black', lineend = 'square'),
-        axis.text = element_text(color = 'black', size = 9),
-        panel.grid = element_blank(),
-        axis.ticks = element_line(color = 'black'),
-        complete = TRUE) +
+  theme_paper() +
     scale_y_continuous(expand = c(0, 0))
 }
 
